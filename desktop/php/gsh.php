10171297d98627abbe798da5dfa8802ecdc9e348
Loïc <loic@jeedom.com>
2019-01-30 18:22:41 +0100

    Fix sorter

--- desktop/php/gsh.php
+++ desktop/php/gsh.php
@@ -21,70 +21,70 @@ if (!isConnect('admin')) {
 					<th>{{Plugin}}</th>
 					<th data-sorter="false" data-filter="false">{{Options}}</th>
 					<th>{{Status}}</th>
-					<th>{{Type}}</th>
-					<th>{{Pseudo}}</th>
+					<th data-sorter="select-text">{{Type}}</th>
+					<th data-sorter="inputs">{{Pseudo}}</th>
 					<th data-sorter="false" data-filter="false">{{Action}}</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
-foreach (eqLogic::all() as $eqLogic) {
-	echo '<tr class="device" data-link_id="' . $eqLogic->getId() . '" data-link_type="eqLogic">';
-	echo '<td>' . $eqLogic->getHumanName(true) . '</td>';
-	echo '<td>' . $eqLogic->getEqType_name() . '</td>';
-	echo '<td>';
-	echo '<input style="display:none;" class="deviceAttr" data-l1key="id" />';
-	echo '<input style="display:none;" class="deviceAttr" data-l1key="link_type" value="eqLogic" />';
-	echo '<input style="display:none;" class="deviceAttr" data-l1key="link_id" value="' . $eqLogic->getId() . '" />';
-	echo '<div class="row">';
-	echo '<div class="col-lg-6">';
-	echo '<input type="checkbox" class="deviceAttr" data-l1key="enable" /> <label>{{Transmettre}}</label><br/>';
-	echo '</div>';
-	echo '<div class="col-lg-6">';
-	echo '<select class="deviceAttr form-control input-sm" data-l1key="options" data-l2key="challenge">';
-	echo '<option value="">{{Aucune}}</option>';
-	//echo '<option value="ackNeeded">{{Validation}}</option>';
-	echo '<option value="pinNeeded">{{Code}}</option>';
-	echo '</select>';
-	echo '</div>';
-	echo '<div class="col-lg-6">';
-  echo '<input type="checkbox" class="deviceAttr" data-l1key="options" data-l2key="reportState" /> <label>{{Remonter l\'état}}</label>';
-	echo '</div>';
-	echo '<div class="col-lg-6">';
-	echo '<input class="deviceAttr form-control input-sm challenge pinNeeded" data-l1key="options" data-l2key="challenge_pin" placeholder="{{Code pin}}" />';
-	echo '</div>';
-	echo '</div>';
-	echo '</td>';
-	echo '<td>';
-	echo '<span class="deviceAttr label" data-l1key="options" data-l2key="configState" style="font-size:1em;"></span>';
-	echo '</td>';
-	echo '<td>';
-	echo '<select class="deviceAttr form-control input-sm" data-l1key="type">';
-	echo '<option value="">{{Aucun}}</option>';
-	foreach (gsh::$_supportedType as $key => $value) {
-		if ($key == 'action.devices.types.SCENE') {
-			continue;
-		}
-		echo '<option value="' . $key . '">{{' . $value['name'] . '}}</option>';
-	}
-	echo '<select>';
-	echo '</td>';
-	echo '<td>';
-	echo '<input class="deviceAttr form-control input-sm" data-l1key="options" data-l2key="pseudo" />';
-	echo '</td>';
-	echo '<td style="width:150px;">';
-	echo ' <a class="btn btn-success btn-sm bt_advanceConfigureEqLogic" data-id="' . $eqLogic->getId() . '" ><i class="fas fa-cog"></i></a>';
-	echo ' <a class="btn btn-default btn-sm bt_configureEqLogic" data-id="' . $eqLogic->getId() . '"><i class="fa fa-cogs"></i></a>';
-	echo ' <a class="btn btn-default btn-sm" href="' . $eqLogic->getLinkToConfiguration() . '"  target="_blank"><i class="fa fa-external-link"></i></a>';
-	echo '</td>';
-	echo '</tr>';
-}
-?>
+				foreach (eqLogic::all() as $eqLogic) {
+					echo '<tr class="device" data-link_id="' . $eqLogic->getId() . '" data-link_type="eqLogic">';
+					echo '<td>' . $eqLogic->getHumanName(true) . '</td>';
+					echo '<td>' . $eqLogic->getEqType_name() . '</td>';
+					echo '<td>';
+					echo '<input style="display:none;" class="deviceAttr" data-l1key="id" />';
+					echo '<input style="display:none;" class="deviceAttr" data-l1key="link_type" value="eqLogic" />';
+					echo '<input style="display:none;" class="deviceAttr" data-l1key="link_id" value="' . $eqLogic->getId() . '" />';
+					echo '<div class="row">';
+					echo '<div class="col-lg-6">';
+					echo '<input type="checkbox" class="deviceAttr" data-l1key="enable" /> <label>{{Transmettre}}</label><br/>';
+					echo '</div>';
+					echo '<div class="col-lg-6">';
+					echo '<select class="deviceAttr form-control input-sm" data-l1key="options" data-l2key="challenge">';
+					echo '<option value="">{{Aucune}}</option>';
+					//echo '<option value="ackNeeded">{{Validation}}</option>';
+					echo '<option value="pinNeeded">{{Code}}</option>';
+					echo '</select>';
+					echo '</div>';
+					echo '<div class="col-lg-6">';
+					echo '<input type="checkbox" class="deviceAttr" data-l1key="options" data-l2key="reportState" /> <label>{{Remonter l\'état}}</label>';
+					echo '</div>';
+					echo '<div class="col-lg-6">';
+					echo '<input class="deviceAttr form-control input-sm challenge pinNeeded" data-l1key="options" data-l2key="challenge_pin" placeholder="{{Code pin}}" />';
+					echo '</div>';
+					echo '</div>';
+					echo '</td>';
+					echo '<td>';
+					echo '<span class="deviceAttr label" data-l1key="options" data-l2key="configState" style="font-size:1em;"></span>';
+					echo '</td>';
+					echo '<td>';
+					echo '<select class="deviceAttr form-control input-sm" data-l1key="type">';
+					echo '<option value="">{{Aucun}}</option>';
+					foreach (gsh::$_supportedType as $key => $value) {
+						if ($key == 'action.devices.types.SCENE') {
+							continue;
+						}
+						echo '<option value="' . $key . '">{{' . $value['name'] . '}}</option>';
+					}
+					echo '<select>';
+					echo '</td>';
+					echo '<td>';
+					echo '<input class="deviceAttr form-control input-sm" data-l1key="options" data-l2key="pseudo" />';
+					echo '</td>';
+					echo '<td style="width:150px;">';
+					echo ' <a class="btn btn-success btn-sm bt_advanceConfigureEqLogic" data-id="' . $eqLogic->getId() . '" ><i class="fas fa-cog"></i></a>';
+					echo ' <a class="btn btn-default btn-sm bt_configureEqLogic" data-id="' . $eqLogic->getId() . '"><i class="fa fa-cogs"></i></a>';
+					echo ' <a class="btn btn-default btn-sm" href="' . $eqLogic->getLinkToConfiguration() . '"  target="_blank"><i class="fa fa-external-link"></i></a>';
+					echo '</td>';
+					echo '</tr>';
+				}
+				?>
 			</tbody>
 		</table>
 	</div>
-
-
+	
+	
 	<div role="tabpanel" class="tab-pane" id="scenariotab">
 		<a class="btn btn-success pull-right btn-xs" id="bt_addScene" style="margin-top:5px;"><i class="fa fa-plus"></i> {{Ajouter scène}}</a>
 		<br/><br/>
