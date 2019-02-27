59401348b65548c84266ac72482ce19d6f5566d7
Loïc <loic@jeedom.com>
2019-02-27 10:23:59 +0100

    prevent warning

--- core/class/gsh_securitysystem.class.php
+++ core/class/gsh_securitysystem.class.php
@@ -182,6 +182,9 @@ public static function getState($_device, $_infos) {
 			$value = $cmd->execCmd();
 			if(isset($_infos['attributes']['availableModes']) && count($_infos['attributes']['availableModes']) > 0){
 				foreach ($_infos['attributes']['availableModes'] as $mode) {
+					if(!isset($mode['name']){
+						continue;
+					}
 					$found = null;
 					if(isset($mode['settings']) && count($mode['settings']) > 0){
 						foreach ($mode['settings'] as $setting) {
