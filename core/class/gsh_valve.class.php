a35db97685009bb770cf29a1947ae1e366accf22
Loïc <loic@jeedom.com>
2019-05-28 12:45:22 +0200

    bugfix

--- core/class/gsh_valve.class.php
+++ core/class/gsh_valve.class.php
@@ -19,7 +19,7 @@
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
-class gsh_curtian {
+class gsh_valve {
 	
 	/*     * *************************Attributs****************************** */
 	
