ecf7f56dbad6e67183fc5ad43e2e821cd4534ae3
Loïc <loic@jeedom.com>
2019-10-15 12:22:48 +0200

    update

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -1,20 +1,20 @@
 <?php
 
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
@@ -37,6 +37,11 @@ function gsh_install() {
 		config::save('gshs::useragent', rand(100, 10000), 'gsh');
 	}
 	jeedom::getApiKey('gsh');
+	try {
+		gsh::sendJeedomConfig();
+	} catch (\Exception $e) {
+		
+	}
 }
 
 function gsh_update() {
@@ -59,7 +64,7 @@ function gsh_update() {
 }
 
 function gsh_remove() {
-
+	
 }
 
 ?>
