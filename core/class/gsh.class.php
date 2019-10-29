c28ba12634e0414a83b2fa3cab0844a957e2e66d
Loïc <loic@jeedom.com>
2019-10-29 09:29:42 +0100

    add switch type

--- core/class/gsh.class.php
+++ core/class/gsh.class.php
@@ -43,6 +43,7 @@ class gsh extends eqLogic {
 		'action.devices.types.LIGHT' => array('class' => 'gsh_light', 'name' => 'Lumière'),
 		'action.devices.types.THERMOSTAT' => array('class' => 'gsh_thermostat', 'name' => 'Thermostat'),
 		'action.devices.types.OUTLET' => array('class' => 'gsh_outlet', 'name' => 'Prise'),
+		'action.devices.types.SWITCH' => array('class' => 'gsh_outlet', 'name' => 'Interrupteur'),
 		'action.devices.types.CAMERA' => array('class' => 'gsh_camera', 'name' => 'Caméra'),
 		'action.devices.types.SCENE' => array('class' => 'gsh_scene', 'name' => 'Scene'),
 		'action.devices.types.BLINDS' => array('class' => 'gsh_blinds', 'name' => 'Store'),
