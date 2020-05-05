5149129c1a18592794cc0822a621eecf14110850
Loïc <loic@jeedom.com>
2020-05-05 17:16:29 +0200

    try

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -7,7 +7,7 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 sudo apt update
 sudo apt install -y npm
 cd ${BASEDIR}/gshd
-npm install
+sudo npm install
 echo 95 > /tmp/dependancy_gsh_in_progress
 
 echo 100 > /tmp/dependancy_gsh_in_progress
