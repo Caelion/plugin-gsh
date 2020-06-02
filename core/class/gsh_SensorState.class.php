e88d5eafc60e4380082ec1ac54b6918c338f5c3e
Loïc <loic@jeedom.com>
2020-06-02 14:52:39 +0200

    continue

--- core/class/gsh_SensorState.class.php
+++ core/class/gsh_SensorState.class.php
@@ -22,113 +22,50 @@ require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 class gsh_SensorState {
 	
 	/*     * *************************Attributs****************************** */
-	private static $_CONVERSION = array(
-		'TEMPERATURE' => array(
-			'name' => 'temperature',
-			'data_type' => array(array('type_synonym'=>  array('température'), 'lang'=> 'fr')),
-			'default_device_unit' => '°C',
-		),
-		'HUMIDITY' =>array(
-			'name' => 'humidity',
-			'data_type' => array(array('type_synonym'=>  array('humidité'), 'lang'=> 'fr')),
-			'default_device_unit' => '%',
-		),
-		'AIR_QUALITY' => array(
-			'name' => 'air_quality_co2',
-			'data_type' => array(array('type_synonym'=>  array('qualité de l\'air'), 'lang'=> 'fr')),
-			'default_device_unit' => 'ppm',
-		),
-		'DEPTH' => array(
-			'name' => 'depth',
-			'data_type' => array(array('type_synonym'=>  array('profondeur'), 'lang'=> 'fr')),
-			'default_device_unit' => 'm',
-		),
-		'WIND_DIRECTION' => array(
-			'name' => 'direction',
-			'data_type' => array(array('type_synonym'=>  array('Direction du vent'), 'lang'=> 'fr')),
-			'default_device_unit' => '',
-		),
-		'CONSUMPTION' => array(
-			'name' => 'energy_usage',
-			'data_type' => array(array('type_synonym'=>  array('Consommation'), 'lang'=> 'fr')),
-			'default_device_unit' => 'kWh',
-		),
-		'SPEED' => array(
-			'name' => 'speed',
-			'data_type' => array(array('type_synonym'=>  array('Vitesse'), 'lang'=> 'fr')),
-			'default_device_unit' => 'km/h',
-		),
-		'DISTANCE' => array(
-			'name' => 'distance',
-			'data_type' => array(array('type_synonym'=>  array('Distance'), 'lang'=> 'fr')),
-			'default_device_unit' => 'm',
-		)
-	);
+	
+	private $_SMOKE = array('SMOKE');
+	private $_WATER_LEAK = array('WATER_LEAK');
+	
+	
 	/*     * ***********************Methode static*************************** */
 	
 	public static function discover($_eqLogic) {
 		$return = array('traits' => array(),'customData' => array(),'attributes' => array());
 		$return['attributes']['dataTypesSupported'] = array();
-		$modes = '';
 		foreach ($_eqLogic->getCmd() as $cmd) {
-			if (in_array($cmd->getGeneric_type(), array('TEMPERATURE'))) {
-				$return['customData']['SensorState_cmdGetTemperature'] = $cmd->getId();
+			if (in_array($cmd->getGeneric_type(), $_SMOKE)) {
+				$return['customData']['SensorState_cmdGetSmokeLevel'] = $cmd->getId();
 				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
 					$return['traits'][] = 'action.devices.traits.SensorState';
+					$return['attributes']['sensorStatesSupported'][] = array(
+						'name' => 'SmokeLevel',
+						'descriptiveCapabilities' => array(
+							'availableStates' => array('smoke detected','no smoke detected')
+						)
+					);
 				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['TEMPERATURE'];
 			}
-			if (in_array($cmd->getGeneric_type(), array('HUMIDITY'))) {
-				$return['customData']['SensorState_cmdGetHumidity'] = $cmd->getId();
+			if (in_array($cmd->getGeneric_type(), $_WATER_LEAK)) {
+				$return['customData']['SensorState_cmdGetWaterLeak'] = $cmd->getId();
 				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
 					$return['traits'][] = 'action.devices.traits.SensorState';
+					$return['attributes']['sensorStatesSupported'][] = array(
+						'name' => 'WaterLeak',
+						'descriptiveCapabilities' => array(
+							'availableStates' => array('leak','no leak')
+						)
+					);
 				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['HUMIDITY'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('AIR_QUALITY'))) {
-				$return['customData']['SensorState_cmdGetAirQuality'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['AIR_QUALITY'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('DEPTH'))) {
-				$return['customData']['SensorState_cmdGetDepth'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['DEPTH'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('WIND_DIRECTION','WEATHER_WIND_DIRECTION'))) {
-				$return['customData']['SensorState_cmdGetWindDirection'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['WIND_DIRECTION'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('CONSUMPTION'))) {
-				$return['customData']['SensorState_cmdGetConsumption'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['CONSUMPTION'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('SPEED','WEATHER_WIND_SPEED'))) {
-				$return['customData']['SensorState_cmdGetSpeed'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['SPEED'];
-			}
-			if (in_array($cmd->getGeneric_type(), array('DISTANCE'))) {
-				$return['customData']['SensorState_cmdGetDistance'] = $cmd->getId();
-				if (!in_array('action.devices.traits.SensorState', $return['traits'])) {
-					$return['traits'][] = 'action.devices.traits.SensorState';
-				}
-				$return['attributes']['dataTypesSupported'][] = self::$_CONVERSION['DISTANCE'];
 			}
 		}
 		return $return;
+	}
+	
+	public static function needGenericType(){
+		return array(
+			__('Fumée',__FILE__) => self::$_SMOKE,
+			__('Fuite d\'eau',__FILE__) => self::$_WATER_LEAK
+		);
 	}
 	
 	public static function exec($_device, $_executions, $_infos) {
@@ -141,8 +78,7 @@ class gsh_SensorState {
 		$return['online'] = true;
 		$return['on'] = true;
 		$eqLogic = $_device->getLink();
-		
-		$return['currentSensorData'] = array();
+		$return['currentSensorStateData'] = array();
 		
 		foreach ($_infos['customData'] as $key => $cmd_id) {
 			$cmd = cmd::byId($cmd_id);
@@ -150,58 +86,20 @@ class gsh_SensorState {
 				continue;
 			}
 			$type = strtolower(str_replace('SensorState_cmdGet','',$key));
+			$value = $cmd->execCmd();
 			switch ($type) {
-				case 'temperature':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'temperature',
-					'default_device_units' => '°C',
-					'data_value' => $cmd->execCmd(),
-				);
+				case 'SmokeLevel':
+				$value = ($value == 1) ? 'smoke detected'  : 'no smoke detected';
 				break;
-				case 'humidity':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'humidity',
-					'data_value' => $cmd->execCmd(),
-				);
-				break;
-				case 'depth':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'depth',
-					'data_value' => $cmd->execCmd(),
-				);
-				break;
-				case 'air_quality':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'air_quality_co2',
-					'data_value' => $cmd->execCmd(),
-				);
-				break;
-				case 'consumption':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'energy_usage',
-					'data_value' => $cmd->execCmd(),
-				);
-				break;
-				case 'speed':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'speed',
-					'data_value' => $cmd->execCmd(),
-				);
-				break;
-				case 'distance':
-				$return['currentSensorData'][] = array(
-					'name' =>self::$_CONVERSION[strtoupper($type)]['name'],
-					'data_type_key' => 'distance',
-					'data_value' => $cmd->execCmd(),
-				);
+				case 'WaterLeak':
+				$value = ($value == 1) ? 'leak'  : 'no leak';
 				break;
 			}
+			
+			$return['currentSensorStateData'][] = array(
+				'name' => $key,
+				'currentSensorState' =>  $value
+			);
 		}
 		return $return;
 	}
