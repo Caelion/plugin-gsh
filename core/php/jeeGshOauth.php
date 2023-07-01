ef224c2bf06656d2fb0fd01dcf1750082d47bcb1
Loïc <loic@jeedom.com>
2023-07-01 16:49:19 +0200

    Update jeeGshOauth.php

--- core/php/jeeGshOauth.php
+++ core/php/jeeGshOauth.php
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
-<?php
 require_once __DIR__ . '/../../../../core/php/core.inc.php';
 
 if (init('response_type') == 'code') {
