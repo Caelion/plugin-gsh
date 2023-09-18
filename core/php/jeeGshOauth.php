4e3cec599819189fb7e8ab024cba9cf340ca289c
Loïc <1536036+zoic21@users.noreply.github.com>
2023-09-18 13:27:31 +0200

    Update jeeGshOauth.php

--- core/php/jeeGshOauth.php
+++ core/php/jeeGshOauth.php
@@ -17,11 +17,6 @@
 require_once __DIR__ . '/../../../../core/php/core.inc.php';
 
 if (init('response_type') == 'code') {
-	// Allow access from local network only
-	if (network::getUserLocation() != 'internal'){
-		echo 'You are no comming from internal address';
-		die();
-	}
 	include_file('core', 'authentification', 'php');
 	// Check for a logged in user
 	if (!isConnect()) {
